ceb33aee081534b56a5c7ea1b79d99dea84eaf01
Serdar Polat <serdar.plt21@gmail.com>
2022-06-24 00:34:09 +0300

    scrollnotify & backdrop

--- lib/widget_extensions.dart
+++ lib/widget_extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 extension PaddingExtensions on Widget {
@@ -28,5 +30,35 @@ extension PaddingExtensions on Widget {
   Padding paddingFromLTRB(double left, double top, double right, double bottom) => Padding(
         padding: EdgeInsets.fromLTRB(left, top, right, bottom),
         child: this,
+      );
+}
+
+extension Scrollnotify on Widget {
+  Widget scrollListener() => NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          if (scrollNotification is ScrollStartNotification) {
+            print(scrollNotification.metrics);
+          } else if (scrollNotification is ScrollUpdateNotification) {
+            print(scrollNotification.metrics);
+          } else {
+            print(scrollNotification.metrics);
+          }
+
+          return true;
+        },
+        child: this,
+      );
+}
+
+extension Backdrop on Widget {
+  Widget backdropFilter({double? borderRadius, double? sigmaX, double? sigmaY}) => ClipRRect(
+        borderRadius: borderRadius != null ? BorderRadius.circular(borderRadius) : BorderRadius.zero,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: sigmaX ?? 10,
+            sigmaY: sigmaY ?? 10,
+          ),
+          child: this,
+        ),
       );
 }
