c4e6ba65b2c2bc576788453bf7b6290c9d3f1b7c
Serdar Polat <serdar.plt21@gmail.com>
2022-06-24 01:17:03 +0300

    Random item

--- lib/list_extensions.dart
+++ lib/list_extensions.dart
@@ -1,3 +1,9 @@
+import 'dart:math' as math show Random;
+
 extension Subscript<T> on Iterable<T> {
   T? operator [](int index) => length > index ? elementAt(index) : null;
+}
+
+extension RandomItem<T> on Iterable<T> {
+  T getRandomItem() => elementAt(math.Random().nextInt(length));
 }
