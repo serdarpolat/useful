18ba88d6ff51876a5f289cda2b52d2930f6cb333
serdarpolat <serdar.plt21@gmail.com>
2022-07-01 00:00:13 +0300

    Bezier

--- lib/func.dart
+++ lib/func.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
 class Func {
   Func._();
 
@@ -29,10 +32,44 @@ class Func {
       if ((i * sublistCount) + sublistCount > list.length) {
         newList.add(list.sublist(i * sublistCount));
       } else {
-        newList.add(list.sublist(i * sublistCount, (i * sublistCount) + sublistCount));
+        newList.add(
+            list.sublist(i * sublistCount, (i * sublistCount) + sublistCount));
       }
     }
 
     return newList;
+  }
+
+  Offset quadraticCurve(
+    Offset start,
+    Offset end,
+    Offset control,
+    double t,
+  ) {
+    return Offset(
+        Math.pow((1 - t), 2) * start.dx +
+            2 * (1 - t) * t * control.dx +
+            Math.pow(t, 2) * end.dx,
+        Math.pow((1 - t), 2) * start.dy +
+            2 * (1 - t) * t * control.dy +
+            Math.pow(t, 2) * end.dy);
+  }
+
+  Offset cubicCurve(
+    Offset start,
+    Offset end,
+    Offset control1,
+    Offset control2,
+    double t,
+  ) {
+    return Offset(
+        Math.pow((1 - t), 3) * start.dx +
+            3 * Math.pow((1 - t), 2) * t * control1.dx +
+            3 * (1 - t) * Math.pow(t, 2) * control2.dx +
+            Math.pow(t, 3) * end.dx,
+        Math.pow((1 - t), 3) * start.dy +
+            3 * Math.pow((1 - t), 2) * t * control1.dy +
+            3 * (1 - t) * Math.pow(t, 2) * control2.dy +
+            Math.pow(t, 3) * end.dy);
   }
 }
